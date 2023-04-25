93199874b890d3d66fdf7f680f01039d038f9138
ronshalit00 <ronshalit00@gmail.com>
2023-04-25 13:20:41 +0300

    Add functional calculator with click listeners

--- index.js
+++ index.js
@@ -14,20 +14,85 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
-let firstNum, operator, secondNum;
+let firstNum = null,
+	operator = null,
+	secondNum = null,
+	firstAndOperator = null,
+	dotPressed = false;
 
 function operate(firstNum, operator, secondNum) {
-	let result;
-	if (operator === "plus") {
-		result = add(firstNum, secondNum);
-	} else if (operator === "minus") {
-		result = subtract(firstNum, secondNum);
-	} else if (operator === "x") {
-		result = multiply(firstNum, secondNum);
-	} else if (operator === "division") {
-		result = divide(firstNum, secondNum);
+	switch (operator) {
+		case "plus":
+			return add(firstNum, secondNum);
+		case "minus":
+			return subtract(firstNum, secondNum);
+		case "x":
+			return multiply(firstNum, secondNum);
+		case "division":
+			if (secondNum != 0) {
+				return divide(firstNum, secondNum);
+			} else {
+				alert("You can't fool me! Don't divide by 0");
+				return firstNum;
+			}
 	}
 }
 
 const screen = document.querySelector("#screen");
-const btnContainer = document.querySelector("#btn-container");
+const buttons = document.querySelectorAll("#btn-container div");
+let displayContent = "";
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		if (button.id == "equals") {
+			if (firstNum && operator) {
+				secondNum = Number(displayContent.replace(firstAndOperator, ""));
+				displayContent = Number(
+					operate(firstNum, operator, secondNum).toFixed(5)
+				);
+				secondNum = null;
+				operator = null;
+				firstNum = null;
+				dotPressed = false;
+			}
+		} else if (button.id == "ac") {
+			displayContent = "";
+			firstNum = null;
+			operator = null;
+			secondNum = null;
+			dotPressed = false;
+		} else if (button.className == "operator") {
+			if (firstNum === null) {
+				//First operator click
+				firstNum = +displayContent;
+				operator = button.id;
+				displayContent += ` ${button.innerText} `;
+				firstAndOperator = displayContent;
+				dotPressed = false;
+			} else if (secondNum === null) {
+				secondNum = Number(displayContent.replace(firstAndOperator, ""));
+				firstNum = Number(operate(firstNum, operator, secondNum).toFixed(5));
+				secondNum = null;
+				operator = button.id;
+				firstAndOperator = `${firstNum} ${button.innerText} `;
+				displayContent = firstAndOperator;
+				dotPressed = false;
+			}
+		} else if (button.id == "dot") {
+			switch (dotPressed) {
+				case false:
+					displayContent += `${button.textContent}`;
+					dotPressed = true;
+					break;
+				case true:
+					break;
+			}
+		} else {
+			//Tap on number
+			displayContent += `${button.textContent}`;
+		}
+		screen.textContent = displayContent;
+	});
+});
+
+function calculate() {}
